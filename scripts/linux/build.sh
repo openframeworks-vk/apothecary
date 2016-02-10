59c55df5f102485f24f981af7fa1eadad879d34b
arturo castro <arturo@openframeworks.cc>
2016-02-10 19:53:52 +0100

    linux scripts: upload files after compiling

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -1,3 +1,12 @@
 APOTHECARY_PATH=$(dirname "$0")/../../apothecary
 cd $APOTHECARY_PATH
 ./apothecary update core
+cd ..
+TARBALL=openFrameworksLibs_$(git rev-parse --abbrev-ref HEAD).tar.bz2
+tar cjf $TARBALL $(ls  | grep -v apothecary | grep scripts)
+openssl aes-256-cbc -K $encrypted_aa785955a938_key -iv $encrypted_aa785955a938_iv -in scripts/id_rsa.enc -out id_rsa -d
+cp scripts/ssh_config ~/.ssh/config
+chmod 600 scripts/id_rsa
+scp -i scripts/id_rsa $TARBALL tests@ci.openframeworks.cc:libs/$TARBALL.new
+ssh -i scripts/id_rsa tests@ci.openframeworks.cc "mv libs/$TARBALL.new libs/$TARBALL"
+rm id_rsa
